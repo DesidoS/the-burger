b784097a38065aec950c12f4f6817a1f27e0a2d6
Oleksii Kravchenko <djdesidos@gmail.com>
2023-04-03 15:18:30 +0300

    add-blog-link

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -3,8 +3,11 @@
     <Container>
       <div class="header__content">
         <router-link to="/homepage">
-          <Logo />
+          <Logo class="header__logo" />
         </router-link>
+        <a href="https://blog-a.herokuapp.com/" class="header__link">
+          <p class="header__p">Блог новинок!</p>
+        </a>
         <p v-if="name">Welcome, {{ name }}! Choose you burger.</p>
       </div>
     </Container>
@@ -45,6 +48,26 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+
+  &__logo {
+    transition: all 0.3s ease;
+  }
+  &__logo:hover {
+    transform: scale(1.2);
+  }
+  &__link {
+    text-decoration: none;
+    color: #333;
+    padding: 8px 16px;
+    transition: all 0.3s ease;
+  }
+  &__link:hover {
+    transform: scale(1.2);
+  }
+  &__p {
+    font-size: 20px;
+    font-weight: 700;
   }
 }
 </style>
